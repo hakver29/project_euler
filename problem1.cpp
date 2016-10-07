25cff0f7eb3b800ab3372b737ad9a75a4cf21599
hakver29 <haakongv@hotmail.com>
2016-10-07 21:31:58 +0200

    Create problem1.cpp

---
+++ problem1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int S = 0;
+    for(int i = 0; i < 1000; i++){
+        if(i % 3 == 0 or i % 5 == 0)
+            S += i;
+    }
+    cout << S << endl;
