0c5921640b6e33206e1ab88eb8c10b74784ee0cc
haako <haakon.veraas@overvinne.no>
2020-12-01 19:56:05 +0100

    finished problem96

--- python/problem96.py
+++ python/problem96.py
@@ -1,22 +1,108 @@
-def square_and_multiply(rest,base,power,modulo):
-    if power == 1:
-        return base*rest
-    if power % 2 == 0:
-        rest = rest
-        base = (base**2) % modulo
-        power = power / 2
-        return square_and_multiply(rest,base,power,modulo)
-    elif power % 2 == 1:
-        rest = rest*base % modulo
-        base = (base**2) % modulo
-        power = (power - 1)/2
-        return square_and_multiply(rest, base,power,modulo)
 
-def main():
-    base = 2
-    power = 7830457
-    modulo = 10000000000
-    rest = 28433
-    X = square_and_multiply(rest,base,power,modulo) % modulo
-    X += 1
-    return X
+def read_boards():
+    boards = []
+
+    with open("p096_sudoku.txt") as f:
+        content = f.readlines()
+
+    #content = map(lambda s: s.strip("\n"), content)
+    for i in range(len(content)):
+        if i % 10 == 0:
+            board = []
+            for j in range(i+1,i+10):
+                row = content[j].strip("\n")
+                row_map = map(int, row)
+                row_list = list(row_map)
+                board.append(row_list)
+
+            boards.append(board)
+    print(boards)
+
+    return boards
+
+
+board = [
+    [7,8,0,4,0,0,1,2,0],
+    [6,0,0,0,7,5,0,0,9],
+    [0,0,0,6,0,1,0,7,8],
+    [0,0,7,0,4,0,2,6,0],
+    [0,0,1,0,5,0,9,3,0],
+    [9,0,4,0,6,0,0,0,5],
+    [0,7,0,3,0,0,0,1,2],
+    [1,2,0,0,0,7,4,0,0],
+    [0,4,9,2,0,6,0,0,7]
+]
+
+
+def solve(bo):
+    find = find_empty(bo)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1,10):
+        if valid(bo, i, (row, col)):
+            bo[row][col] = i
+
+            if solve(bo):
+                return True
+
+            bo[row][col] = 0
+
+    return False
+
+
+def valid(bo, num, pos):
+    # Check row
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # Check column
+    for i in range(len(bo)):
+        if bo[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # Check box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x * 3, box_x*3 + 3):
+            if bo[i][j] == num and (i,j) != pos:
+                return False
+
+    return True
+
+
+def print_board(bo):
+    for i in range(len(bo)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - ")
+
+        for j in range(len(bo[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(bo[i][j])
+            else:
+                print(str(bo[i][j]) + " ", end="")
+
+
+def find_empty(bo):
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
+            if bo[i][j] == 0:
+                return (i, j)  # row, col
+
+    return None
+
+boards = read_boards()
+S = 0
+for i in boards:
+    solve(i)
+    S += i[0][0]*100 + i[0][1]*10 + i[0][2]
+
+print(S)
